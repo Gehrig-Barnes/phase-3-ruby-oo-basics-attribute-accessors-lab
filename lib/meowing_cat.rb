33f9b42e41a20ff0f3e5b0951a059168a8b7858d
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-01 10:22:41 -0600

    commit tree e5239e84e31120ac0f962888bba74cb6a048e02c

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+class Cat
+    attr_accessor :name 
+
+    def meow
+        puts "meow!" 
+    end
+end
+
+gehrig = Cat.new
+gehrig.name = "gehrig"
+puts gehrig.name
+puts gehrig.meow
